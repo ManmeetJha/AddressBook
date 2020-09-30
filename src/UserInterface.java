ceb48e6fdc77ec6fbb4dd7cc8c4e6a8b968bdb66
Navneet <navneetjh5@gmail.com>
2020-09-30 10:13:19 +0530

    Adding new contact

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,8 +1,70 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
 
+	public static List<Address> addressbook;
+
 	public static void main(String[] args) {
-		System.out.println("**Welcome to ADDRESS BOOK**");
+	
+		Scanner input = new Scanner(System.in);
+		addressbook = new ArrayList<Address>();
+		int choice = 0;
+		do {
+			System.out.println("Select option:");
+			System.out.println("1. Add contact");
+			System.out.println("2. Edit contact");
+			System.out.println("3. Delete contact");
+			System.out.println("4. Exit");
+
+			choice = input.nextInt();
+			switch (choice) {
+			case 1:
+				insertIntoAddressBook(input);
+				break;
+			case 2:
+				//editAddress(input);
+				break;
+			case 3:
+				//deleteContact(input);
+				break;
+			default:
+				break;
+			}
+		} while (choice != 4);
 	}
 
+	public static void insertIntoAddressBook(Scanner input) {
+		System.out.println("Enter user details to update");
+		System.out.println("Enter first name:");
+		String first_name = input.next();
+		System.out.println("Enter last name:");
+		String last_name = input.next();
+		System.out.println("Enter address");
+		String address = input.next();
+		System.out.println("Enter city:");
+		String city = input.next();
+		System.out.println("Enter state:");
+		String state = input.next();
+		System.out.println("Enter zip:");
+		String zip = input.next();
+		System.out.println("Enter phone no:");
+		String phone_no = input.next();
+		System.out.println("Enter email:");
+		String email = input.next();
+
+		Address newAddress = new Address(first_name, last_name, address, city, state, zip, phone_no, email);
+		addressbook.add(newAddress);
+		System.out.println("Address added successfully");
+		printAddressBook();
+	}
+
+	static void printAddressBook() {
+		for (int i = 0; i < addressbook.size(); i++) {
+			Address currentAddress = addressbook.get(i);
+			System.out.println(currentAddress.toString());
+		}
+	}
 }
